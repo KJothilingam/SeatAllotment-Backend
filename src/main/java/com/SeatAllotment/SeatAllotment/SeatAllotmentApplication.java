44a81c5a242c3b6edef24220761d18462a8abeea
Lingam <kjothilingam06@gmail.com>
2025-03-17 13:40:27 +0530

    Final

--- src/main/java/com/SeatAllotment/SeatAllotment/SeatAllotmentApplication.java
+++ src/main/java/com/SeatAllotment/SeatAllotment/SeatAllotmentApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeatAllotmentApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SeatAllotmentApplication.class, args);
 	}
